2a9b291b9aff64eaec5319e330cd1934d3b37434
waichong <wctan4444@yahoo.com>
2019-03-23 16:26:59 +0800

    Initial Project Add

---
+++ src/main/java/twc/springframework/twcpetclinic/TwcPetClinicApplication.java
@@ -0,0 +1,13 @@
+package twc.springframework.twcpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwcPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwcPetClinicApplication.class, args);
+    }
+
+}
